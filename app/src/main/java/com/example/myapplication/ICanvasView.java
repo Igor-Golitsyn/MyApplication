2cd3d672e959a75aecf1dc69b60339986090ad72
Igor Golitsyn <polus79@mail.ru>
2016-08-08 13:11:27 +0500

    fin 7 lesson

--- app/src/main/java/com/example/myapplication/ICanvasView.java
+++ app/src/main/java/com/example/myapplication/ICanvasView.java
@@ -4,5 +4,5 @@ package com.example.myapplication;
  * Created by Игорь on 08.08.2016.
  */
 public interface ICanvasView {
-    void drawCircle(MainCircle circle);
+    void drawCircle(SimpleCircle circle);
 }
---
+++ app/src/main/java/com/example/myapplication/SimpleCircle.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+/**
+ * Created by Игорь on 08.08.2016.
+ */
+public class SimpleCircle {
+    protected int x;
+    protected int y;
+    protected int radius;
+
+    public SimpleCircle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
